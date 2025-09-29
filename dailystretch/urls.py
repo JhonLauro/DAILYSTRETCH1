f77c4d0d2b4b2deb2d1ec3d89d1723bfea4b2dae
John Kheinzy Mandawe <kheinzy.rocket@gmail.com>
2025-09-29 17:45:56 +0800

    Login Register(backend)

--- dailystretch/urls.py
+++ dailystretch/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from dailystretch_app import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('home/', views.home_view, name='home'),
+    path('logout/', views.logout_view, name='logout'),
 ]
